c9abe3b065a87e3ddb822b2cdaf9ffbf8edd1de0
Adrian Przerwa <adrian.przerwae@gmail.com>
2026-03-05 06:17:58 +0100

    chore: cleanup duplicate JS and CSS files

--- src/components/auth-provider.js
+++
@@ -1,5 +0,0 @@
-"use client";
-import { Fragment as _Fragment, jsx as _jsx } from "react/jsx-runtime";
-export default function AuthProvider({ children, }) {
-    return _jsx(_Fragment, { children: children });
-}
